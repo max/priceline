50735713736d3bf0f8c7f58305ca26a6b9749598
Max Schoening <mail@maximilian.io>
2014-10-04 15:07:26 -0700

    Add support for Handlebar template compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var concat = require('gulp-concat');
 var mainBowerFiles = require('main-bower-files');
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
@@ -25,13 +28,31 @@ gulp.task('javascripts', function () {
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', ['stylesheets', 'bower', 'javascripts']);
+gulp.task('templates', function() {
+  gulp.src('./client/templates/**/*.hbs')
+    .pipe(handlebars({
+      handlebars: require('ember-handlebars')
+    }))
+    .pipe(wrap('Ember.Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'Ember.TEMPLATES',
+      noRedeclare: true,
+      processName: function(filePath) {
+        return declare.processNameByPath(filePath.replace('./client/templates/', ''));
+      }
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', ['stylesheets', 'bower', 'javascripts', 'templates']);
 
 gulp.task('watch', function() {
   gulp.watch('./views/**/*', reload);
   gulp.watch('./client/stylesheets/*', ['stylesheets']);
   gulp.watch('./bower_components/**/*', ['bower', reload]);
   gulp.watch('./client/javascripts/*', ['javascripts', reload]);
+  gulp.watch('./client/templates/**/*', ['templates', reload]);
 });
 
 gulp.task('browser-sync', function() {
